ef945052355cd262bc41955bed87e451b7f460c5
Jongchan Lee <mail@fis.to>
2022-01-31 16:37:42 +0900

    Update live date

--- pages/live.tsx
+++ pages/live.tsx
@@ -17,8 +17,8 @@ const LivePage = () => {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content="박영훈 ♡ 김현주 결혼식 중계" />
-        <meta property="og:description" content="10월 3일에 결혼합니다." />
+        <meta property="og:title" content="이종찬 ♡ 이현경 결혼식 중계" />
+        <meta property="og:description" content="4월 9일에 결혼합니다." />
         <meta
           property="og:image"
           content="https://drive.google.com/uc?id=1lF_ryhpAgqZ60Ho9XWYCulxlokcuaVhj"
