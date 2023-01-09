cd2e3e0ca3fd56c5c5fad779fd0205d92abd892a
studiotj <122099377+studiotj@users.noreply.github.com>
2023-01-09 19:46:30 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,24 @@ const HomePage = () => {
   return (
     <>
       <Head>
-        <title>이종찬 ♡ 이현경</title>
+        <title>최태준 ♡ 이슬이</title>
         <meta
           name="description"
-          content="이종찬 ♡ 이현경 4월 9일에 결혼합니다."
+          content="최태준 ♡ 이슬이 2월 12일에 결혼합니다."
         />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content="이종찬 ♡ 이현경 청첩장" />
+        <meta property="og:title" content="최태준 ♡ 이슬이 청첩장" />
         <meta
           property="og:description"
-          content="4월 9일 17시에 결혼합니다."
+          content="2월 12일 18시에 결혼합니다."
         />
         <meta
           property="og:image"
-          content="https://drive.google.com/uc?id=1F7iGw2AJVJXfVa8TSMqQ_r8a3C_psPib"
+          content="https://drive.google.com/file/d/1Cy4Op1Bqi0NmQetDa8SUjO0oUt3mllNt/view?usp=drivesdk"
         />
       </Head>
       <Home />
