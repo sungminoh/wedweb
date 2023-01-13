4aa4a770c5735c21b8d3fae11fb6ade25499b502
studiotj <122099377+studiotj@users.noreply.github.com>
2023-01-13 15:05:32 +0900

    Update index.tsx

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -252,6 +252,12 @@ const GiveWrap = styled.div`
   line-height: 2;
 `;
 
+const MapWrap = styled.div`
+  display: inline-block;
+  text-align: left;
+  line-height: 2;
+`;
+
 const CopyTextButton = styled.button`
   padding: 0;
   border: none;
@@ -627,12 +633,14 @@ const Home = () => {
         <br />
       </p>
    
+      <MapWrap>
+        <strong>자가용 이용시</strong> <br /> 
        <p>
-          자가용 이용시<br />
           분당 방면:한남대교→장충단길→신라호텔 후문→서울신라호텔 <br />
           강남 방면:동호대교→장충체육관 앞 사거리에서 비보호 좌회전→서울신라호텔 <br />
           용산 방면:남산2호터널 통과 후 좌회전→신라호텔 후문→서울신라호텔 <br />
        </p>
+      </MapWrap>
       <br />
       
       
