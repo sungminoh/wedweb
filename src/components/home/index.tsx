54b6562244f82936504f404be566ce53c748cb40
studiotj <122099377+studiotj@users.noreply.github.com>
2023-01-13 16:29:52 +0900

    Update index.tsx

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,7 +1,7 @@
 import { Copy, EmojiLookLeft, EmojiLookRight, PinAlt } from "iconoir-react";
 import Image from "next/image";
 import Link from "next/link";
-import React, {
+import React, { 
   MouseEventHandler,
   useCallback,
   useEffect,
