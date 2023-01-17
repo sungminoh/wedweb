a999a3651d65ab68c4c45316e85dfd09cda6eb6b
studiotj <122099377+studiotj@users.noreply.github.com>
2023-01-17 15:01:45 +0900

    Update live.tsx

--- pages/live.tsx
+++ pages/live.tsx
@@ -7,21 +7,21 @@ const LivePage = () => {
   return (
     <>
       <Head>
-        <title>이종찬 ♡ 이현경 결혼식 중계</title>
+        <title>최태준 ♡ 이슬이 결혼식 중계</title>
         <meta
           name="description"
-          content="이종찬 ♡ 이현경 4월 9일에 결혼합니다."
+          content="최태준 ♡ 이슬이 2월 12일에 결혼합니다."
         />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content="이종찬 ♡ 이현경 결혼식 중계" />
-        <meta property="og:description" content="4월 9일에 결혼합니다." />
+        <meta property="og:title" content="최태준 ♡ 이슬이 결혼식 중계" />
+        <meta property="og:description" content="2월 12일에 결혼합니다." />
         <meta
           property="og:image"
-          content="https://drive.google.com/uc?id=1lF_ryhpAgqZ60Ho9XWYCulxlokcuaVhj"
+          content="https://drive.google.com/uc?id=1N8Kx9ac2KtCGgheNP6GDrTAWknU4XlC-"
         />
       </Head>
       <Live />
@@ -35,7 +35,7 @@ let liveUrlCache = { url: "", when: 0 };
 const getLiveUrl = async () => {
   if (liveUrlCache.when < Date.now() - 10000) {
     const liveUrl = await fetch(
-      "https://drive.google.com/uc?id=1lF_ryhpAgqZ60Ho9XWYCulxlokcuaVhj"
+      "https://drive.google.com/uc?id=1N8Kx9ac2KtCGgheNP6GDrTAWknU4XlC-"
     )
       .then((r) => r.text())
       .then((url) => url.trim());
