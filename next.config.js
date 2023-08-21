6e2cd18424c278530ed537a7096269b628f937fd
sungminoh <smoh2044@gmail.com>
2023-08-20 21:08:55 -0700

    disable dotenv

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const result = require('dotenv').config()
+// const result = require('dotenv').config()
 
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   optimizeFonts: false,
-  env: result.parsed
+  // env: result.parsed
   // eslint: {
   //   // Warning: This allows production builds to successfully complete even if
   //   // your project has ESLint errors.
