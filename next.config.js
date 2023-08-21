c4ef66e99a37856e7b7009b0db8a45ac808b39d6
sungminoh <smoh2044@gmail.com>
2023-08-20 20:09:28 -0700

    dotenv for multiline env variable from vercel (ref. https://github.com/vercel/next.js/discussions/37935)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const result = require('dotenv').config()
+
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   optimizeFonts: false,
+  env: result.parsed
   // eslint: {
   //   // Warning: This allows production builds to successfully complete even if
   //   // your project has ESLint errors.
